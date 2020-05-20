8351d03fc71675e8073e6750b3db8382dbd99756
choigone <gowoonvv@gmail.com>
2020-05-20 23:28:04 +0900

    Updates

---
+++ precache-manifest.35d0cff2c317b5c4e0e995f458b6a70f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09786faed3bac79c4add5a0141b677d1",
+    "url": "/nomad_movie_app/index.html"
+  },
+  {
+    "revision": "179da09a2b5b13f8b279",
+    "url": "/nomad_movie_app/static/css/main.82ee3213.chunk.css"
+  },
+  {
+    "revision": "89c76c6b0b0507fbf6d9",
+    "url": "/nomad_movie_app/static/js/2.6d25c9f5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nomad_movie_app/static/js/2.6d25c9f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179da09a2b5b13f8b279",
+    "url": "/nomad_movie_app/static/js/main.3678997c.chunk.js"
+  },
+  {
+    "revision": "91415d2cf9586deaaa97",
+    "url": "/nomad_movie_app/static/js/runtime-main.07de21bc.js"
+  }
+]);
